e2e29b2f55a99f461b2a74b3a53113b8d9a3b180
dev-truly <kllliv50@gmail.com>
2023-08-03 10:06:00 +0900

    Initial commit

---
+++ src/main/java/tests/Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero");
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println("Addition: " + calculator.add(5, 3));
+        System.out.println("Subtraction: " + calculator.subtract(5, 3));
+        System.out.println("Multiplication: " + calculator.multiply(5, 3));
+        System.out.println("Division: " + calculator.divide(5, 3));
+    }
+
+}
---
+++ src/test/java/com/example/testapplication/CalculatorTests.java
@@ -0,0 +1,2 @@
+package com.example.testapplication;public class CalculatorTests {
+}
